f72f6e0b0048b0e8eebe19a300bef60a6e474457
Jamie <jamierayhutchings@gmail.com>
2021-04-15 16:13:25 +1200

    Fancy doc strings

--- elastic_extract.py
+++ elastic_extract.py
@@ -16,6 +16,8 @@ TODO:
     * This reads environment variables etc. No need for outputt paths or anything
 
   * Improve docstrings to functions...
+  * Add JSON save to file
+  *
 
 """
 import argparse
@@ -40,10 +42,16 @@ class Query:
         self.out_fields.append(self.paging_time_field)
 
 
-def _get_env_variables():
-    """ Load the environment variables.
+def _get_env_variables() -> str, str, str, str:
+    """ Load and return the environment variables.
+
+    Returns:
+        str: env var 'ELASTIC_HOST'
+        str: env var 'ELASTIC_PORT'
+        str: env var 'ELASTIC_USER'
+        str: env var 'ELASTIC_SECRET'
     """
-    load_dotenv()  # Load variables from .env into system environment
+    load_dotenv()  # Load variables from .env file into system environment
     host = os.getenv("ELASTIC_HOST")
     port = os.getenv("ELASTIC_PORT")
     username = os.getenv("ELASTIC_USER")
@@ -52,8 +60,25 @@ def _get_env_variables():
     return host, port, username, password
 
 
-def _generate_query_json(search_fields:list, search_string:str, field_to_exist:str = None, date_field:str = "created_at", start_date:str = None, end_date:str = None, is_match_all:bool = False):
+def _generate_query_json(search_fields:list, search_string:str, field_to_exist:str = None, date_field:str = "created_at", start_date:str = None, end_date:str = None, is_match_all:bool = False) -> dict:
     """ Generates the query json body from the simple query parameters.
+
+    Args:
+        search_fields (list): the fields you want to search for your query string in
+        search_string (str): the terms you want to search for in the search fields
+        field_to_exist (str): supplied field will be used as an extra check to 
+            only return documents where this field isn't null
+        date_field (str): supplied field will be used to search by a custom date field
+            use in conjunction with start_date and end_date args
+        start_date (str): the first date you want to return documents from in format
+            yyyy-mm-dd
+        end_date (str): the last date you want to return documents from in format
+            yyyy-mm-dd, can also be set to 'now' to use current date
+        is_match_all (bool): this overrides search terms and exist terms and returns
+            all documents between start and end dates if specified
+
+    Returns:
+        dict: the json query body used to make API calls
     """
     json = {"query": {'bool': {}}}
 
@@ -79,8 +104,14 @@ def _generate_query_json(search_fields:list, search_string:str, field_to_exist:s
     return json
 
 
-def _args_to_query(args):
+def _args_to_query(args:argparse.ArgumentParser) -> Query:
     """ Take the arguments and return a Query object.
+
+    Args:
+        args (argparse.ArgumentParser): the arguments passed to the program
+
+    Returns:
+        Query: the query object used to call API functions
     """
     json = None
     if args.search != None:
@@ -103,9 +134,36 @@ def _args_to_query(args):
     return query
 
 
-def _get_arguments():
+def _check_arguments(args:argparse.ArgumentParser) -> bool:
+    """ Check that arguments are valid and fail early if not.
+
+    Args:
+        args (argparse.ArgumentParser): the arguments passed to the program
+
+    Returns:
+        bool: True if passes argument checks, False if fails
+    """
+    if  not args.search and not args.exists and not args.match_all:
+        print("Eerror: you must provide a search term")
+        return False
+    elif (args.start and not args.end) or (not args.start and args.end):
+        print("error: you must provide both a start and an end date")
+        return False
+    elif not os.path.exists(os.path.dirname(args.out)) and os.path.dirname(args.out) != '':
+        print(f"error: the directory '{os.path.dirname(args.out)}' does not exist.")
+        return False
+    
+    os.get
+
+    return True
+
+
+def _get_arguments() -> argparse.ArgumentParser:
     """ Parse the command line arguments and return them as an argument
     object.
+
+    Returns:
+        argparse.ArgumentParser: the arguments passed to the program
     """
     parser = argparse.ArgumentParser("Elasticsearch query tool")
     parser.add_argument("-m", "--match_all", help="Match all fields, downloads all data", action="store_true", default=False)
@@ -125,14 +183,18 @@ def _get_arguments():
                         metavar="date (yyyy-mm-dd)", default=None)
     parser.add_argument("-ed", "--end", help="Ending date to stop searching yyyy-mm-dd or now",
                         metavar="date (yyyy-mm-dd)", default=None)
-    parser.add_argument("-o", "--out", help="Output file", default="output.csv")
+    parser.add_argument("-o", "--out", help="Output file with path", default="output.csv")
     args = parser.parse_args()
 
     return args
 
 
-def _write_csv_headers(headers:list, out_file:str):
+def _write_csv_headers(headers:list, out_file:str) -> None:
     """ Write the return fields to the csv file as headers
+
+    Args:
+        headers (list): list of DataFrame column headers (all return fields)
+        out_file (str): the path including filename of the file to write to
     """
     print(f"Writing headers to {out_file}")
     with open(out_file, "w", newline="") as f:
@@ -140,9 +202,19 @@ def _write_csv_headers(headers:list, out_file:str):
         writer.writerow(headers)
 
 
-def _process_response(hits:list, id_field:str, time_field:str):
-    """ Extract the actual document data from the raw response and take care of
-    pagination.
+def _get_docs_from_response(hits:list, id_field:str, time_field:str) -> list, str, str:
+    """ Extract the actual document data from the raw response and extract
+    pagination information.
+
+    Args:
+        hits (list): list of raw response json objects from elasticsearch
+        id_field (str): the id pagination field to update
+        time_field (str): the time pagination field to update
+
+    Returns:
+        list: a list of document data extracted from raw responses
+        str: the latest timestamp to update pagination
+        str: the latest id to update pagination
     """
     timestamp = None
     _id = None
@@ -157,8 +229,17 @@ def _process_response(hits:list, id_field:str, time_field:str):
     return docs, timestamp, _id
 
 
-def _process_json(sources:list, fields:list):
-    """ Clean the document json data from elasticsearch.
+def _clean_elastic_docs(sources:list, fields:list) -> list:
+    """ Clean the document json data from elasticsearch. This cleaning
+    includes inserting empty strings for nulls and unnesting
+    nested fields.
+
+    Args:
+        sources (list): list of document data (from _source object) extracted from raw responses
+        fields (list): list of fields in the data
+
+    Returns:
+        list: a list of documents that have been cleaned for export
     """
     parsed_sources = []
     for source in sources:
@@ -192,10 +273,17 @@ def _process_json(sources:list, fields:list):
     return parsed_sources
 
 
-def _query_to_csv_large(host:str, port:str, username:str, password:str, query:Query, out_file:str):
+def _query_to_csv_large(host:str, port:str, username:str, password:str, query:Query, out_file:str) -> None:
     """ This is the internal function for handling the connection to elasticsearch and
     the subsequent API calls with the data provided from the Query object. It then saves
     the results to a csv file.
+
+    Args:
+        host (str): the elasticsearch host address
+        port (str): the elasticsearch port
+        username (str): the elasticsearch username login
+        password (str): the elasticsearch password
+        query (Query): query object containing desired query
     """
     es = Elasticsearch([host], http_auth=(username, password), scheme="https", port=port,
                     verify_certs=False, ssl_show_warn=False)  # Open connection to the Elasticsearch database
@@ -231,11 +319,11 @@ def _query_to_csv_large(host:str, port:str, username:str, password:str, query:Qu
             current_count += len(res_docs)
             print(f"Downloading: [{current_count}/{document_count}]")
 
-            elastic_docs, last_timestamp, last_id = _process_response(res_docs, query.paging_id_field, query.paging_time_field)  # Extracts data from nested JSON
+            elastic_docs, last_timestamp, last_id = _get_docs_from_response(res_docs, query.paging_id_field, query.paging_time_field)  # Extracts data from nested JSON
 
             query.json["search_after"] = [last_timestamp, last_id]  # Set page marker to last result
 
-            rows = _process_json(elastic_docs, query.out_fields)
+            rows = _clean_elastic_docs(elastic_docs, query.out_fields)
             writer.writerows(rows)
 
     if current_count > 0:
@@ -250,6 +338,16 @@ def _query_to_json(host:str, port:str, username:str, password:str, query:Query) 
     """ This is the internal function for handling the connection to elasticsearch and
     the subsequent API calls with the data provided from the Query object. It then returns
     the resultts in a list of JSON objects.
+
+    Args:
+        host (str): the elasticsearch host address
+        port (str): the elasticsearch port
+        username (str): the elasticsearch username login
+        password (str): the elasticsearch password
+        query (Query): query object containing desired query
+
+    Returns:
+        list: a list of cleaned json documents returned by the query
     """
     print("Connecting to elasticsearch")
 
@@ -283,21 +381,40 @@ def _query_to_json(host:str, port:str, username:str, password:str, query:Query) 
         current_count += len(res_docs)
         print(f"Downloading: [{current_count}/{document_count}]")
 
-        elastic_docs, last_timestamp, last_id = _process_response(res_docs, query.paging_id_field, query.paging_time_field)  # Extracts data from nested JSON
+        elastic_docs, last_timestamp, last_id = _get_docs_from_response(res_docs, query.paging_id_field, query.paging_time_field)  # Extracts data from nested JSON
 
         query.json["search_after"] = [last_timestamp, last_id]  # Set page marker to last result
 
-        rows += _process_json(elastic_docs, query.out_fields)
+        rows += _clean_elastic_docs(elastic_docs, query.out_fields)
 
     print("Done")
 
     return rows
 
 
-def query_to_json(index:str = None, return_fields:list = [], fields_to_search:list = [], search_string:str = None, field_to_exist:str = None, date_field:str = "created_at", start_date:str = None, end_date:str = None, is_match_all:bool = False):
+def query_to_json(index:str = None, return_fields:list = [], fields_to_search:list = [], search_string:str = None, field_to_exist:str = None, date_field:str = "created_at", start_date:str = None, end_date:str = None, is_match_all:bool = False) -> list:
     """ This is the function that takes in query parameters and returns a list of json objects from
     elasticsearch documents. This function creates a query object and calls an internal function that handles
     the actual communication with elasticsearch.
+
+    Args:
+        index (str): the elasticsearch index you want to query
+        return_fields (list): the fields you want returned from the query
+        fields_to_search (list): the fields you want to search for your query string in
+        search_string (str): the terms you want to search for in the search fields
+        field_to_exist (str): supplied field will be used as an extra check to 
+            only return documents where this field isn't null
+        date_field (str): supplied field will be used to search by a custom date field
+            use in conjunction with start_date and end_date args
+        start_date (str): the first date you want to return documents from in format
+            yyyy-mm-dd
+        end_date (str): the last date you want to return documents from in format
+            yyyy-mm-dd, can also be set to 'now' to use current date
+        is_match_all (bool): this overrides search terms and exist terms and returns
+            all documents between start and end dates if specified
+
+    Returns:
+        list: a list of cleaned json documents returned by the query
     """
     host, port, username, password = _get_env_variables()
 
@@ -314,10 +431,29 @@ def query_to_json(index:str = None, return_fields:list = [], fields_to_search:li
     return response_json
 
 
-def query_to_dataframe(index:str = None, return_fields:list = [], fields_to_search:list = [], search_string:str = None, field_to_exist:str = None, date_field:str = "created_at", start_date:str = None, end_date:str = None, is_match_all:bool = False):
+def query_to_dataframe(index:str = None, return_fields:list = [], fields_to_search:list = [], search_string:str = None, field_to_exist:str = None, date_field:str = "created_at", start_date:str = None, end_date:str = None, is_match_all:bool = False) -> pandas.DataFrame:
     """ This is the function that takes in query parameters and returns a pandas datafram from
     elasticsearch documents. This function creates a query object and calls an internal function that handles
     the actual communication with elasticsearch.
+
+    Args:
+        index (str): the elasticsearch index you want to query
+        return_fields (list): the fields you want returned from the query
+        fields_to_search (list): the fields you want to search for your query string in
+        search_string (str): the terms you want to search for in the search fields
+        field_to_exist (str): supplied field will be used as an extra check to 
+            only return documents where this field isn't null
+        date_field (str): supplied field will be used to search by a custom date field
+            use in conjunction with start_date and end_date args
+        start_date (str): the first date you want to return documents from in format
+            yyyy-mm-dd
+        end_date (str): the last date you want to return documents from in format
+            yyyy-mm-dd, can also be set to 'now' to use current date
+        is_match_all (bool): this overrides search terms and exist terms and returns
+            all documents between start and end dates if specified
+
+    Returns:
+        pandas.DataFrame: a DataFrame where the json fields are columns
     """
     host, port, username, password = _get_env_variables()
 
@@ -335,8 +471,15 @@ def query_to_dataframe(index:str = None, return_fields:list = [], fields_to_sear
     return df
 
 
-def write_dataframe_to_file(df:pandas.DataFrame, path:str, format:str="csv"):
+def write_dataframe_to_file(df:pandas.DataFrame, path:str, format:str="csv") -> None:
     """ This function takes a dataframe and exports it to either JSON or CSV.
+    NOTE: This function could be put in the Julia wrapper?
+
+    Args:
+        df (pandas.DataFrame): the dataframe to be stored to file
+        path (str): the path including filename for the output
+        format (str): the format of the file on disk either 'json' 
+            or 'csv'
     """
     if format == "json":
         json = df.to_json(orient="records")
@@ -346,13 +489,19 @@ def write_dataframe_to_file(df:pandas.DataFrame, path:str, format:str="csv"):
         df.to_csv(path, index=False)
 
 
-def read_dataframe_from_file(df:pandas.DataFrame, path:str):
+def read_dataframe_from_file(df:pandas.DataFrame, path:str) -> None:
     """ Function to read in either a json formated file or csv into a dataframe.
+    NOTE: This function could be put in the Julia wrapper?
+
+    Args:
+        df (pandas.DataFrame): the dataframe to be stored to file
+        path (str): the path including filename for the output
     """
-    pass
+    df = pandas.read_csv(path)
+    return df
 
 
-def main():
+def main() -> None:
     """This is the main function which reads the env vars (DISCUSS) and parsees command line args.
     Look up 'arg pass'
     1. Read arguments and fail
@@ -363,11 +512,18 @@ def main():
     host, port, username, password = _get_env_variables()
 
     args = _get_arguments()
+    if not _check_arguments(args):
+        print('Done')
+        return
     output_file = args.out
     query = _args_to_query(args)
 
     _query_to_csv_large(host, port, username, password, query, output_file)
+
+    # Testing external functions here
+    
     #print(query_to_dataframe(fields_to_search=['full_text'], search_string='vaccine', field_to_exist='entities.urls.expanded_url'))
+    
     #print(query_to_json(fields_to_search=['full_text'], search_string='vaccine', field_to_exist='entities.urls.expanded_url'))
 
     #df = query_to_dataframe(fields_to_search=['full_text'], search_string='vaccine', field_to_exist='entities.urls.expanded_url')
